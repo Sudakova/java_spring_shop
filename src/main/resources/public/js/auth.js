18890266c9ad36e190866242744ab6395b92f4be
Andrii Brelyk <abrelyk@smarttech.com>
2019-11-15 15:03:24 +0200

    - add login

--- src/main/resources/public/js/auth.js
+++ src/main/resources/public/js/auth.js
@@ -2,11 +2,13 @@ var basicURL = "http://localhost:8080/";
 
 var userId;
 
-function login() {
+function login(e) {
     console.log("Login");
+    e.preventDefault();
     var login = $("#email").val();
     var password = $("#password").val();
-    var url = basicURL+"login";
+    console.log(login + password);
+    var url = basicURL + "login";
     var fd = new FormData();
     fd.append("email", login);
     fd.append("password", password);
@@ -17,7 +19,7 @@ function login() {
         data: fd
     })
         .done(function (response) {
-           userId = response;
+            console.log(response);
         })
         .fail(function (e) {
             alert("Error  to login. " + e.responseJSON.message)
